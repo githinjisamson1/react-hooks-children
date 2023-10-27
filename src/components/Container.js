fc20cc907434d833ec2edd98304bca69916594eb
Samson Githinji <githinjisamson148@gmail.com>
2023-10-27 05:39:31 +0300

    Completed assignment

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-function Container({
-  header,
-  children,
-  textPosition = "", // possible values: left, right, center
-  direction = "horizontal", // possible values: vertical, horizontal
-  contentPosition = "center", // possible values: left, right, center
-}) {
+function Container(props) {
+  const {
+    header,
+    children,
+    textPosition = "", // possible values: left, right, center
+    direction = "horizontal", // possible values: vertical, horizontal
+    contentPosition = "center", // possible values: left, right, center
+  } = props;
+  
   return (
     <div className={`container ${textPosition}`}>
       {header ? <h2>{header}</h2> : null}
